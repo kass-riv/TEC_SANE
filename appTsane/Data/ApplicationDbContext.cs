6a3e18189ceee663417a6def3483081d0a840ca3
Neyser Tacanga <tacangan@gmail.com>
2021-06-05 01:34:02 -0500

    Modificando Proforma y Producto

--- appTsane/Data/ApplicationDbContext.cs
+++ appTsane/Data/ApplicationDbContext.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using appTsane.Models;
+
 
 namespace appTsane.Data
 {
